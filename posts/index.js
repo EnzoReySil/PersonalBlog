bddc37c88eb9e6ee50ec4b510f29d2bd29ff957e
Juan Lorenzo Reyes Silva <lorenzoreyes62@aragon.unam.mx>
2022-11-01 20:37:31 -0600

    Initial commit

---
+++ posts/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.json());
+
+const posts = {};
+const {randomBytes} = require('crypto');
+
+
+app.get('/posts', (req, res) => {
+    res.send(posts);
+});
+
+app.post('/posts', (req, res) => {
+    const id = randomBytes(4).toString('hex');
+    const {title} = req.body;
+    posts[id] = {
+        id, title
+    }
+    res.status(201).send(posts[id]);
+});
+
+app.listen(4000, () => {
+    console.log('Listening on port 4000');
+});
